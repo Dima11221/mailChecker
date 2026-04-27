800f8300f42b30ecee54365e379de962970d1309
Dima <mistik1321@yandex.ru>
2026-04-27 23:46:53 +0300

    фикс

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -26,7 +26,7 @@ export const MAIL_FOLDERS = ("INBOX,Spam,Junk")
   .split(",")
   .map((value) => value.trim())
   .filter(Boolean);
-export const MAIL_SOURCES = ("banki.ru,irecommend.ru,zoon.ru,2gis.ru,id.yandex.ru")
+export const MAIL_SOURCES = ("banki.ru,irecommend.ru,zoon.ru,2gis.ru,yandex.ru")
   .split(",")
   .map((value) => value.trim())
   .filter(Boolean);
