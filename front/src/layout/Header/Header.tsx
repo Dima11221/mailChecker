f89e3e5052a740cfcb536d625146660aba3f9c7f
Dima <mistik1321@yandex.ru>
2026-05-14 22:57:58 +0300

    fix_style

--- front/src/layout/Header/Header.tsx
+++ front/src/layout/Header/Header.tsx
@@ -1,9 +1,10 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import style from "./style.module.scss"
 import {useAuth} from "../../auth/useAuth.ts";
 
 const Header = () => {
 	const { signOut, user } = useAuth();
+	const {pathname} = useLocation();
 
 	const handleLogout = () => {
 		signOut();
@@ -14,13 +15,13 @@ const Header = () => {
 			<h1>Страницы</h1>
 			<ul className={style.main}>
 				<li>
-					<Link to="/">Главная</Link>
+					<Link to="/" className={pathname === '/' ? style.active : style.mainPage}>Главная</Link>
 				</li>
 				<li>
-					<Link to="/razmeshenie">Размещение</Link>
+					<Link to="/razmeshenie" className={pathname === '/razmeshenie' ? style.active : style.mainPage}>Размещение</Link>
 				</li>
 				<li>
-					<Link to="/sozdanie">Создание</Link>
+					<Link to="/sozdanie" className={pathname === '/sozdanie' ? style.active : style.mainPage}>Создание</Link>
 				</li>
 			</ul>
 			<div>
